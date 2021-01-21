e38a1a568f08cce327cf5832aa82224071f67f06
윤혜민 <h2m0@swu.ac.kr>
2021-01-21 17:51:14 +0900

    :hammer:[EDIT] add calendar router #S04P12A301-29

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -4,6 +4,7 @@ import Login from '../views/user/Login.vue'
 import Signup from '../views/user/Signup.vue'
 import Board from '../views/board/Board.vue'
 import Create from '../views/board/Create.vue'
+import Calendar from '../views/CalendarPage.vue'
 import Detail from '../views/board/Detail.vue'
 
 Vue.use(VueRouter)
@@ -25,6 +26,11 @@ const routes = [
     path: '/board',
     name: 'Board',
     component: Board
+  },
+  {
+    path: '/calendar',
+    name: 'Calendar',
+    component: Calendar
   },
   {
     path: '/board/create',
