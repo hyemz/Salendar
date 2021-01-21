596d9b8631311aaa5284b4b1375c79af8832a328
현성섭 <gustjdtjq@gmail.com>
2021-01-21 14:41:19 +0900

    :bulb:[ADD] board page basic structure #S04P12A301-38

---
+++ frontend/src/views/board/Board.vue
@@ -0,0 +1,260 @@
+<template>
+  <v-container class="">
+    <v-row no-gutters>
+
+      <v-col
+        cols="12"
+        sm="1"
+      >
+        <v-card
+          class="pa-2"
+          outlined
+          tile
+        >
+          1st cloumn
+        </v-card>
+      </v-col>
+
+      <v-col
+        cols="12"
+        sm="10"
+      >
+        <v-card
+          class="pa-2"
+          outlined
+          tile
+        >
+          <div>
+            
+
+            <v-card>
+              <v-card-title>
+                <h1>자유게시판</h1>
+                <v-spacer></v-spacer>
+                <v-spacer></v-spacer>
+                <v-spacer></v-spacer>
+                <v-spacer></v-spacer>
+                <v-spacer></v-spacer>
+                <v-text-field
+                  class="pr-2 pl-14"
+                  v-model="search"
+                  append-icon="mdi-magnify"
+                  label="Search"
+                  single-line
+                  hide-details
+                ></v-text-field>
+
+                <v-btn
+                  tile
+                  color=""
+                  @click="create"
+                >
+                  <v-icon left>
+                    mdi-pencil
+                  </v-icon>
+                  New
+                </v-btn>
+
+              </v-card-title>
+              <v-data-table
+                class="pl-4 pr-4"
+                :headers="headers"
+                :items="contents"
+                :search="search"
+              ></v-data-table>
+            </v-card>
+          </div>
+        </v-card>
+      </v-col>
+
+      <v-col
+        cols="12"
+        sm="1"
+      >
+        <v-card
+          class="pa-2"
+          outlined
+          tile
+        >
+          3rd column
+        </v-card>
+      </v-col>
+
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        search: '',
+        headers: [
+          {
+            text: '게시글 번호',
+            align: 'start',
+            sortable: false,
+            value: 'id',
+          },
+          { text: '작성자', value: 'username', align: 'start',},
+          { text: '제목', value: 'title', align: 'start',},
+          { text: '작성 시간', value: 'created_at', align: 'center',},
+          { text: '수정 시간', value: 'updated_at', align: 'center', },
+        ],
+        contents: [
+          {
+            id: '1',
+            username: '홍길동',
+            title: '동해물과 백두산이 마르고 닳도록 하느님이 보우하사 우리나라',
+            created_at: '12:34:56',
+            updated_at: '12:34:56',
+          },
+          {
+            id: '2',
+            username: '홍길동2',
+            title: 9.0,
+            created_at: '12:34:56',
+            updated_at: '12:34:56',
+          },
+          {
+            id: '3',
+            username: '홍길동3',
+            title: 16.0,
+            created_at: '12:34:56',
+            updated_at: '12:34:56',
+          },
+          {
+            id: '4',
+            username: '홍길동4',
+            title: 3.7,
+            created_at: '12:34:56',
+            updated_at: '12:34:56',
+          },
+          {
+            id: '5',
+            username: '홍길동5',
+            title: 16.0,
+            created_at: '12:34:56',
+            updated_at: '12:34:57',
+          },
+          {
+            id: '6',
+            username: '홍길동6',
+            title: 0.0,
+            created_at: '12:34:56',
+            updated_at: '12:34:56',
+          },
+          {
+            id: '7',
+            username: '홍길동7',
+            title: 0.2,
+            created_at: '12:34:56',
+            updated_at: '12:34:56',
+          },
+          {
+            id: '8',
+            username: '홍길동8',
+            title: 3.2,
+            created_at: '12:34:56',
+            updated_at: '12:34:56',
+          },
+          {
+            id: '9',
+            username: '홍길동9',
+            title: 25.0,
+            created_at: '12:34:56',
+            updated_at: '12:34:56',
+          },
+          {
+            id: '10',
+            username: '홍길동10',
+            title: 26.0,
+            created_at: '12:34:56',
+            updated_at: '12:34:56',
+          },
+          {
+            id: '11',
+            username: '홍길동11',
+            title: '동해물과 백두산이 마르고 닳도록 하느님이 보우하사 우리나라',
+            created_at: '12:34:56',
+            updated_at: '12:34:56',
+          },
+          {
+            id: '12',
+            username: '홍길동12',
+            title: 9.0,
+            created_at: '12:34:56',
+            updated_at: '12:34:56',
+          },
+          {
+            id: '13',
+            username: '홍길동13',
+            title: 16.0,
+            created_at: '12:34:56',
+            updated_at: '12:34:56',
+          },
+          {
+            id: '14',
+            username: '홍길동14',
+            title: 3.7,
+            created_at: '12:34:56',
+            updated_at: '12:34:56',
+          },
+          {
+            id: '15',
+            username: '홍길동15',
+            title: 16.0,
+            created_at: '12:34:56',
+            updated_at: '12:34:57',
+          },
+          {
+            id: '16',
+            username: '홍길동16',
+            title: 0.0,
+            created_at: '12:34:56',
+            updated_at: '12:34:56',
+          },
+          {
+            id: '17',
+            username: '홍길동17',
+            title: 0.2,
+            created_at: '12:34:56',
+            updated_at: '12:34:56',
+          },
+          {
+            id: '18',
+            username: '홍길동18',
+            title: 3.2,
+            created_at: '12:34:56',
+            updated_at: '12:34:56',
+          },
+          {
+            id: '19',
+            username: '홍길동19',
+            title: 25.0,
+            created_at: '12:34:56',
+            updated_at: '12:34:56',
+          },
+          {
+            id: '20',
+            username: '홍길동20',
+            title: 26.0,
+            created_at: '12:34:56',
+            updated_at: '12:34:56',
+          },
+        ],
+      }
+    },
+    methods: {
+      create: function () {
+        this.$router.push({
+          path: '/board/create'
+        })
+      },
+    }
+  }
+</script>
+
+<style>
+
+</style>
---
+++ frontend/src/views/board/Create.vue
@@ -0,0 +1,163 @@
+<template>
+  <v-container class="">
+    <v-row no-gutters>
+
+      <v-col
+        cols="12"
+        sm="1"
+      >
+        <v-card
+          class="pa-2"
+          outlined
+          tile
+        >
+          1st cloumn
+        </v-card>
+      </v-col>
+
+      <v-col
+        cols="12"
+        sm="10"
+      >
+        <v-card
+          class="pa-2"
+          outlined
+          tile
+        >
+          <div>
+            <v-card>
+              <v-card-title>
+                <h1 class="pt-2">새 글 작성하기 / 글 수정하기</h1>
+              </v-card-title>
+              <br>
+              <v-card-text
+              >분류
+              </v-card-text>
+
+              <v-col
+                class="pl-4"
+                cols="12"
+                sm="2"
+                >
+                <v-select
+                :items="category"
+                label="글의 분류를 선택하세요"
+                outlined
+                ></v-select>
+              </v-col>
+
+
+              <v-card-text
+              >제목
+              </v-card-text>
+              
+              <v-text-field
+                class="pl-4 pr-4"
+                label="제목을 입력해 주십시오"
+                :rules="titlerules"
+                hide-details="auto"
+                outlined
+                
+                ></v-text-field>
+
+              <v-card-text
+              >내용
+              </v-card-text>
+
+              <v-textarea
+                class="pl-4 pr-4"
+                label="내용을 입력해 주십시오"
+                :rules="contentrules"
+                hide-details="auto"
+                outlined
+                height=200
+                ></v-textarea>
+
+              <v-card-text
+              >TAG
+              </v-card-text>
+
+              <v-text-field
+                class="pl-4 pr-4"
+                label="태그를 입력해 주십시오"
+                outlined
+                ></v-text-field>
+
+              <v-card-text
+              >파일첨부
+              </v-card-text>
+
+              <v-file-input
+                truncate-length="15"
+                class="pl-5 pr-5"
+                ></v-file-input>
+
+              <v-btn 
+                class="mr-1 ml-4 mb-6"
+                elevation="2"
+                @click="create"
+                color="grey lighten-2"
+                >저장</v-btn>
+              <v-btn 
+                class="mb-6"
+                elevation="2"
+                @click="backtoboard"
+                color="grey lighten-2"
+                >목록</v-btn>
+
+                
+
+            </v-card>
+            
+
+            
+          </div>
+        </v-card>
+      </v-col>
+
+      <v-col
+        cols="12"
+        sm="1"
+      >
+        <v-card
+          class="pa-2"
+          outlined
+          tile
+        >
+          3rd column
+        </v-card>
+      </v-col>
+
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+    data: () => ({
+      titlerules: [
+        value => (value && value.length >= 3) || '최소 3글자 이상 입력해 주세요',
+        value => (value && value.length <= 25) || '25자 이상 입력할 수 없습니다',
+      ],
+      contentrules: [
+        value => (value && value.length >= 3) || '최소 3글자 이상 입력해 주세요',
+        value => (value && value.length <= 50) || '50자 이상 입력할 수 없습니다',
+      ],
+      category: ['자유게시판', '제품 리뷰', '세일 제보 게시판',],
+    }),
+    methods: {
+        backtoboard () {
+          this.$router.push({
+          path: '/board'
+          })
+        },
+        create () {
+            alert("새 글 작성이 완료되었습니다.")
+        }
+    }
+  }
+</script>
+
+<style>
+
+</style>
