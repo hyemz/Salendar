6c835f238450ebf93b53a238d0fa3452582a0fde
hue <euhmai@gmail.com>
2021-01-25 10:43:28 +0900

    :hammer:[EDIT] modify folder and router

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -4,8 +4,12 @@ import Login from '../views/user/Login.vue'
 import Signup from '../views/user/Signup.vue'
 import Board from '../views/board/Board.vue'
 import Create from '../views/board/Create.vue'
+import Calendar from '../views/CalendarPage.vue'
 import Detail from '../views/board/Detail.vue'
+import MyPage from '../views/mypage/MyPage.vue'
 import MyPost from '../views/mypage/MyPost.vue'
+import WishList from '../views/mypage/WishList.vue'
+
 
 Vue.use(VueRouter)
 
@@ -28,6 +32,11 @@ const routes = [
     component: Board
   },
   {
+    path: '/calendar',
+    name: 'Calendar',
+    component: Calendar
+  },
+  {
     path: '/board/create',
     name: 'Create',
     component: Create
@@ -38,11 +47,21 @@ const routes = [
     component: Detail
   },
   {
+    path: '/mypage',
+    name: 'MyPage',
+    component: MyPage
+  },
+  {
     path: '/mypage/mypost',
     name: 'MyPost',
     component: MyPost
   },
-  
+  {
+    path: 'mypage/wishlist',
+    name: 'WishList',
+    component: WishList
+  },
+
 ]
 
 const router = new VueRouter({
