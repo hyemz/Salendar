8989b65c794baa8c94b0554ad6c806286849e02a
YeonggilGo <dudrlf1859@naver.com>
2021-02-01 15:03:44 +0900

    :bug:[EDIT] debug some error again...

--- frontend/src/lib/axiosDefault.js
+++ frontend/src/lib/axiosDefault.js
@@ -4,9 +4,9 @@ const baseURL = "http://localhost:8080";
 const headers = {
   "x-auth-token": localStorage.getItem("jwt"),
 };
-const axiosClient = axios.create({
+const axiosDefault = axios.create({
   baseURL,
   headers,
 });
 
-export default axiosClient;
+export default axiosDefault;
