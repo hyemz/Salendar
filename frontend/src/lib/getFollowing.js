42b77de38d2b5b71a30e291072258e8fe23669e0
윤혜민 <h2m0@swu.ac.kr>
2021-02-02 15:00:31 +0900

    :hammer:[EDIT] connect wishlist backend #S04P13A301-87

--- frontend/src/lib/getFollowing.js
+++ frontend/src/lib/getFollowing.js
@@ -1,14 +1,7 @@
 import axiosClient from "./axiosClient"
 
-const getFollowing =  
-    axiosClient
-          .get("/api/user/token/followings")
-          .then((res) => {
-            console.log(res);
-            // this.datas = res.data;
-          })
-          .catch((err) => {
-            console.log('찜 목록을 불러오지 못했습니다.', err);
-          });
+const getFollowing =
+  axiosClient
+    .get("/api/user/token/followings");
 
 export default getFollowing;
