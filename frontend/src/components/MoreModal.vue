7ff094e420847c500007bf44ae61f094baa2f3f5
윤혜민 <h2m0@swu.ac.kr>
2021-02-19 13:21:45 +0900

    :bug: [EDIT] fix dday error

--- frontend/src/components/MoreModal.vue
+++ frontend/src/components/MoreModal.vue
@@ -123,10 +123,11 @@ export default {
     dialog: function() {
       this.data = this.focus._def.extendedProps;
       console.log(this.focus);
-      const d1 = this.$moment(this.focus.end.getDate() - 1).format('YYYY-MM-DD');
+      const d1 = this.$moment(this.focus.end.setDate(this.focus.end.getDate() - 1)).format(
+        'YYYY-MM-DD'
+      );
       this.dday = this.$moment(d1).diff(this.$moment(), 'days');
       if (this.dday <= 0) {
-        this.dday = '+' + (Math.abs(this.dday) + 1);
       } else if (this.$moment(d1) == this.$moment()) {
         this.dday = '-day';
       } else {
